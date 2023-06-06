90ca1b0bb6ffc43eb3329f60c2f9bb7c0d5c22e8
MAKCKACHKA <doubleakop@gmail.com>
2023-06-06 10:32:04 +0300

    1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryItemsMarkup = galleryItems
+  .map(
+    item => `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${item.original}">
+        <img class="gallery__image" src="${item.preview}" alt="${item.description}">
+      </a>
+    </li>
+`
+  )
+  .join('');
+gallery.innerHTML = galleryItemsMarkup;
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
