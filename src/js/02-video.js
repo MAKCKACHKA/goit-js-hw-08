49b2386655b96376850959407d024a004876ba6a
MAKCKACHKA <doubleakop@gmail.com>
2023-06-06 23:07:05 +0300

    5

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,19 +1,3 @@
-// import Player from '@vimeo/player';
-// const iframe = document.querySelector('iframe');
-// const player = new Player(iframe);
-// var throttle = require('lodash.throttle');
-
-// player.on(
-//   'timeupdate',
-//   throttle(function (event) {
-//     localStorage.setItem('videoplayer-current-time', event.seconds);
-//   }, 1000)
-// );
-
-// const savedTime = localStorage.getItem('videoplayer-current-time');
-// if (savedTime) {
-//   player.setCurrentTime(savedTime);
-// }
 import Player from '@vimeo/player';
 const iframe = document.querySelector('iframe');
 const player = new Player(iframe);
@@ -21,9 +5,12 @@ var throttle = require('lodash.throttle');
 
 player.on(
   'timeupdate',
-  throttle(evt => {
-    localStorage.setItem('videoplayer-current-time', evt.seconds);
+  throttle(event => {
+    localStorage.setItem('videoplayer-current-time', event.seconds);
   }, 1000)
 );
+
 const savedTime = localStorage.getItem('videoplayer-current-time');
+// if (savedTime) {
 player.setCurrentTime(savedTime);
+// }
